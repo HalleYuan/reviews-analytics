e5c95464ebf854257e05cbcc0bcde45d664ab46f
Halle Yuan <halleyuan412@gmail.com>
2021-12-22 16:43:01 +0800

    calc_average_length_reviews

--- read.py
+++ read.py
@@ -1,5 +1,15 @@
 data = []
+count = 0
 with open ('reviews.txt' , 'r') as f:
     for line in f:
         data.append(line)
-        print(len(data))
+        count += 1
+        if count % 1000 == 0: # %是用來求餘數
+            print(len(data))
+print( '檔案讀取完了，總過有', len(data) , '筆資料' )
+
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print( '平均是' , sum_len/len(data) ) #每一筆留言的長度 / 總長
